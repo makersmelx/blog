4202b17f16b66abca43d12cd5138f5c61864bd25
makersmelx <makersmelx@gmail.com>
2023-06-27 20:26:44 -0700

    seo update

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 export const SITE = {
-  title: '名字后头再说',
+  title: '知晓天空之蓝的人呐',
   author: 'makersmelx',
   description: 'makersmelx 的随意日常',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  keywords: 'makersmelx,Travel,Photography,Blog,macOS',
   icon: 'https://s2.loli.net/2023/06/28/Fxjvks2DmbiTIwz.png',
   pic: 'https://s2.loli.net/2023/06/28/Fxjvks2DmbiTIwz.png',
-  homePage: 'https://jiayao.me',
-  blogPage: 'https://jiayao.me',
+  homePage: 'https://randomly.jiayao.me',
+  blogPage: 'https://randomly.jiayao.me',
   twitterId: 'makersmelx',
   githubId: 'makersmelx',
   repo: 'blog'

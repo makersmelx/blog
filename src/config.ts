16e34dc7d57745a481cc4311f161aa7c522d4bd0
makersmelx <makersmelx@gmail.com>
2024-03-29 22:10:19 -0700

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
   title: '知晓天空之蓝的人呐',
   author: 'makersmelx',
-  description: 'makersmelx 的随意日常',
+  description: 'makersmelx 的旅游记录',
   keywords: 'makersmelx,Travel,Photography,Blog,macOS',
-  icon: 'https://s2.loli.net/2023/06/28/Fxjvks2DmbiTIwz.png',
-  pic: 'https://s2.loli.net/2023/06/28/Fxjvks2DmbiTIwz.png',
+  icon: 'https://s2.loli.net/2024/03/30/oOXwkIGhjg9rNc8.jpg',
+  pic: 'https://s2.loli.net/2024/03/30/oOXwkIGhjg9rNc8.jpg',
   homePage: 'https://randomly.jiayao.me',
   blogPage: 'https://randomly.jiayao.me',
   twitterId: 'makersmelx',
